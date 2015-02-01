004b45b8efff2e8b53aa8aab2f1260874ed62929
David McCuskey <david@mentalhijack.com>
2015-02-01 16:40:17 -0700

    update main

--- main.lua
+++ main.lua
@@ -2,138 +2,60 @@
 -- Ghosts vs Monsters sample project, OOP version
 --
 -- OOP version by David McCuskey
--- Original designed and created by Jonathan and Biffy Beebe of Beebe Games exclusively for Ansca, Inc.
+-- Original designed and created by Jonathan & Biffy Beebe
+-- of Beebe Games exclusively for Ansca, Inc.
 --
 -- Sample code is MIT licensed, the same license which covers Lua itself
 -- http://en.wikipedia.org/wiki/MIT_License
--- Copyright (C) 2011 David McCuskey. All Rights Reserved.
+-- Copyright (C) 2011-2015 David McCuskey. All Rights Reserved.
 -- Copyright (C) 2010 ANSCA Inc. All Rights Reserved.
 --====================================================================--
 
 
---====================================================================--
--- Imports
---====================================================================--
 
-local HUDFactory = require( "hud_objects" )
-local director = require("director")
-local levelMgr	-- will be a require, used for testing
+print( '\n\n##############################################\n\n' )
 
--- Uncomment below code and replace init() arguments with valid ones to enable openfeint
---[[
-local openfeint = require ("openfeint")
-openfeint.init( "App Key Here", "App Secret Here", "Ghosts vs. Monsters", "App ID Here" )
-]]--
 
 
 --====================================================================--
--- Setup, Constants
---====================================================================--
-
---Hide status bar from the beginning
-display.setStatusBar( display.HiddenStatusBar )
-
-local appGroup -- groups for all items
-
--- this is used to pass information around to each Director scene
--- ie, no globals
-local app_token = {
-	token_id = 4,
-	mainGroup = nil,
-	hudGroup = nil,
-	loadScreenHUD = nil,
-	gameEngine = nil, -- ref to Game Engine, if it is running
-	--openfeint = openfeint,
-}
-
-
---== Override base functionality
-
-local oldTimerCancel = timer.cancel
-timer.cancel = function(t) if t then oldTimerCancel(t) end end
-
-
---====================================================================--
--- Main
+--== Ghost vs Monsters : main
 --====================================================================--
 
 
+-- Semantic Versioning Specification: http://semver.org/
 
-local function onSystem( event )
+local VERSION = "0.2.0"
 
-	if event.type == "applicationSuspend" then
-		if app_token.gameEngine then
-			app_token.gameEngine:pauseGamePlay()
-		end
 
-	elseif event.type == "applicationExit" then
-		if system.getInfo( "environment" ) == "device" then
-			-- prevents iOS 4+ multi-tasking crashes
-			os.exit()
-		end
+
+--====================================================================--
+--== Setup, Constants
+
+
+local Controller = nil -- set in main()
+
+_G.gMODE = 'TEST'  -- 'TEST'/'RUN'
+
+
+
+--====================================================================--
+--== Main
+--====================================================================--
+
+
+local main = function()
+
+	local params = {
+		-- put here any params required for App or Test
+	}
+	if gMODE == 'TEST' then
+		Controller = require 'test_controller'
+	else
+		Controller = require 'app_controller'
 	end
-end
-
-
--- initialize()
---
-local function initialize()
-
-	-- Create display groups
-	appGroup = display.newGroup()
-	app_token.mainGroup = display.newGroup()
-	app_token.hudGroup = display.newGroup()
-
-	appGroup:insert( app_token.mainGroup )
-	appGroup:insert( app_token.hudGroup )
-
-	-- loading screen
-	local loadScreenHUD = HUDFactory.create( "loadscreen-hud" )
-	app_token.hudGroup:insert( loadScreenHUD.display )
-	app_token.loadScreenHUD = loadScreenHUD
-
-	-- system events
-	Runtime:addEventListener( "system", onSystem )
+	Controller.run( params )
 
 end
 
--- test()
--- test out individual screens
---
-local function test( screen_name, params )
-
-	local test_screen = require( screen_name )
-	test_screen.new( params )
-
-end
-
--- main()
---
-local function main()
-
-	initialize()
-
-	-- Add the group from director class
-	app_token.mainGroup:insert( director.directorView )
-
-	director:changeScene( app_token, "scene-menu" )
-
-end
-
-
--- testing structure
-if ( true ) then
-
-	main()
-
-else
-	levelMgr = require( "level_manager" )
-
-	initialize()
-
-	test( "scene-menu", app_token )
-	app_token.data = levelMgr:getLevelData( 'level1' )
-	--test( "scene-game", app_token )
-
-end
+main()
 
